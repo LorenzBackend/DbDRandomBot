db16be4c189fbcf28a8fccd75e76aa1c4ac9f99e
Lorenz Fresh <73407537+lorenzfresh@users.noreply.github.com>
2021-06-10 19:57:08 +0200

    test

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ client.events = new Discord.Collection();
 })
 
 client.on("ready", () => {
-   client.user.setActivity('-help');
+client.user.setActivity(`!help | ${client.guilds.cache.size} servers`);
     setInterval(() => {
         let id = Math.floor(Math.random() * activites.length);
         client.user.setActivity(activites[id]);
