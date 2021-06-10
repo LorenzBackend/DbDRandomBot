30f693a9b4ebf16954539541de5d7561e90fcc27
Lorenz Fresh <73407537+lorenzfresh@users.noreply.github.com>
2021-06-10 19:23:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -23,4 +23,11 @@ var activites = [
     'Dead by Daylight'
 ]
 
+const channel = guild.channels.cache.find(channel => channel.name === 'general') || guild.channels.cache.first();
+if (channel) {
+    channel.send('DBD Randomizer ');
+} else {
+    console.log('The server ' + guild.name + ' has no channels.');
+}
+
 client.login(process.env.DJS_TOKEN);
